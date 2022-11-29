c4fd3a3ba66fd217668486b987d73271d4c12914
Tran-Hoang-Bao-Nam <118170038+Tran-Hoang-Bao-Nam@users.noreply.github.com>
2022-11-29 19:52:43 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = ' Nấm ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happybirthday';
+const titleCard = 'Gửi Bạn Tui';
+const contentCard = 'Chúc mày một ngày Sinh nhật tuổi 18  thật ý nghĩa, tràn đầy niềm vui và ngập tràn hạnh phúc nhé! Chúc mày luôn mãi cười tươi, cười thật duyên cười thật e thẹn, cười trẻ trung tươi khỏe và cười các thật nhiều kiểu nữa… trong ngày hôm nay. Happy Brthday Hoàng Nghi!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'ALE09424.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
